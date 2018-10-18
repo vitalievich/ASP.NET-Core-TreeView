84e6e45f7a1e58a358e598adf4d3159db7e73e17
samouchka <vitalievichs@gmail.com>
2018-10-18 19:17:32 +0300

    Update testController.cs

--- TreeView/Controllers/testController.cs
+++ TreeView/Controllers/testController.cs
@@ -21,6 +21,7 @@ namespace TreeView.Controllers
             repo = r;
         }
         // Выдача дерева по запросу из клиента (jstree)
+        // Generation tree by client (browser jstree) query 
         public JsonResult GetNodesJsTree(string id)
         {
             GetPaths();
@@ -29,11 +30,13 @@ namespace TreeView.Controllers
         private void GetPaths()
         {
             // получаем строку с открытыми узлами дерева из сессии
+            // getting string with opened nodes fron Session
             if (!HttpContext.Session.Keys.Contains("ONK"))
             {
                 HttpContext.Session.SetString("ONK", "");
             }
             // сохраняем строку в TreeViewPathProvider, одновременно генерируетс массив строк с id открытых узлов
+            // save string inti TreeViewPathProvider, and making string array with id`s of opened nodes
             TreeViewPathProvider.Instance().OpenNodesString = HttpContext.Session.GetString("ONK");
         }
         private void SavePaths()
@@ -44,12 +47,14 @@ namespace TreeView.Controllers
         public void CloseNode(string id)
         {
             // отрабатываем событие в дереве по сворачиванию узла
+            // handle event of closing tree node
             TreeViewPathProvider.Instance().DelNode(id);
             SavePaths();
         }
         public void OpenNode(string id)
         {
             // отрабатываем событие в дереве по разворачиванию узла
+            // handle event of opening tree node
             string parid = repo.Datas.Where(d => d.id == id).Single().parentId;
             TreeViewPathProvider.Instance().AddNode(parid,id);
             SavePaths();
